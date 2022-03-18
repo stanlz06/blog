d2c9a31b7bf0a901a2b2ba3eac67edeb92565c51
linzhenbin <770541591@qq.com>
2022-03-18 17:10:41 +0800

    fix:修改背景图片

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -13,10 +13,10 @@ module.exports = {
   editLinks: true, // 启用编辑
   editLinkText: '编辑',
   bodyBgImg: [
-    '/img/bodyBgImg1.png',
-    '/img/bodyBgImg2.png',
-    '/img/bodyBgImg3.png',
-    '/img/bodyBgImg4.png',
+    'https://cdn.jsdelivr.net/gh/summerking1/image@main/0-5.jpg',
+    'https://cdn.jsdelivr.net/gh/summerking1/image@main/0-2.jpg',
+    'https://cdn.jsdelivr.net/gh/summerking1/image@main/0-3.jpg',
+    'https://cdn.jsdelivr.net/gh/summerking1/image@main/0-6.jpg',
   ],
   bodyBgImgOpacity: 0.7,
 
